4b9803e7bc0a8269c3c014157ebc4bb32c96924e
8i-ght <needboosts3545@gmail.com>
2024-01-31 13:52:12 -0600

    fix possible bugs with ingredients in server

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor(private http: HttpClient, private recipesService: RecipeService) {}
+    
+        storeRecipes() {
+            const recipes = this.recipesService.getRecipes();
+            return this.http.put('https://recipe-app-26905-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(response => {
+                console.log(response);
+            });
+        }
+
+        fetchRecipes() {
+            this.http.get<Recipe[]>('https://recipe-app-26905-default-rtdb.firebaseio.com/recipes.json').pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+                });
+            })).subscribe(recipes => {
+            this.recipesService.setRecipes(recipes);
+        });
+        }
+}
